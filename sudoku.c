20ba37f981446bfb74e8de7bd622b167af8f861f
Your Name <you@example.com>
2024-05-15 19:26:58 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -3,62 +3,111 @@
 #include "list.h"
 
 
-typedef struct{
+typedef struct
+{
    int sudo[9][9];
 }Node;
 
-Node* createNode(){
-  Node* n=(Node*) malloc(sizeof(Node));
+Node* createNode()
+{
+  Node* n = (Node*) malloc(sizeof(Node));
   return n;
 }
 
-Node* copy(Node* n){
-    Node* new=(Node*) malloc(sizeof(Node));
+Node* copy(Node* n)
+{
+    Node* new = (Node*) malloc(sizeof(Node));
     *new = *n;
     return new;
 }
 
-Node* read_file (char* file_name){
-  Node* n = createNode();
-  FILE* file = fopen (file_name, "r");
-  int i,j;
-  for(i=0;i<9;i++){
-       for(j=0;j<9;j++){
-          if(!fscanf (file, "%d", &n->sudo[i][j]))
+Node* read_file (char* file_name)
+{
+   Node* n = createNode();
+   FILE* file = fopen (file_name, "r");
+   int i,j;
+   for(i = 0; i < 9; i++)
+   {
+      for(j = 0; j < 9; j++)
+      {
+         if(!fscanf (file, "%d", &n->sudo[i][j]))
             printf("failed to read data!");
-       }
-  }
+      }
+   }
 
-  fclose (file);
-  return n;
+   fclose (file);
+   return n;
 }
 
-void print_node(Node* n){
-    int i,j;
-    for(i=0;i<9;i++){
-       for(j=0;j<9;j++)
-          printf("%d ", n->sudo[i][j]);
-       printf("\n");
-    }
-    printf("\n");
+void print_node(Node* n)
+{
+   int i,j;
+   for(i = 0; i < 9; i++)
+   {
+      for(j = 0; j < 9; j++)
+      {
+         printf("%d ", n -> sudo[i][j]);
+      }
+      printf("\n");
+   }
+   printf("\n");
 }
 
-int is_valid(Node* n){
-
-    return 1;
+int is_valid(Node* n)
+{
+   int i, j, k;
+   for (i = 0; i < 9; i++) {
+      for (j = 0; j < 9; j++) {
+         if (n -> sudo[0][0] == 0) {
+            continue;
+         }
+         for (k = 0; k < 9; k++) {
+            if (k != j) {
+               if (n -> sudo[i][j] == n -> sudo[i][k]) {
+                  return 0;
+               }
+            }
+         }
+         for (k = 0; k < 9; k++) {
+            if (k != i) {
+               if (n -> sudo[i][j] == n -> sudo[k][j]) {
+                  return 0;
+               }
+            }
+         }
+         int x0 = (i / 3) * 3;
+         int y0 = (j / 3) * 3;
+         for (k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+               if( k + x0 != i && l + y0 != j) {
+                  if(n -> sudo[i][j] == n -> sudo[k + x0][l + y0]) {
+                     return 0;
+                  }
+               }
+            }
+         }
+      }
+   }
+   return 1;
 }
 
 
-List* get_adj_nodes(Node* n) {
+List* get_adj_nodes(Node* n) 
+{
    List* list = createList();
    int i,j;
-   for(i = 0; i < 9; i++) {
-      for(j = 0; j < 9; j++) {
-         if(n -> sudo[i][j] == 0) {
-            for(int k = 1; k <= 9; k++) {
+   for(i = 0; i < 9; i++) 
+   {
+      for(j = 0; j < 9; j++) 
+      {
+         if(n -> sudo[i][j] == 0) 
+         {
+            for(int k = 1; k <= 9; k++) 
+            {
                Node* adj = copy(n);
                adj -> sudo[i][j] = k;
-               if(is_valid(adj)) {
+               if(is_valid(adj)) 
+               {
                   pushBack(list, adj);
                }
             }
@@ -66,7 +115,6 @@ List* get_adj_nodes(Node* n) {
          }
       }
    }
-   
    return list;
 }
 
